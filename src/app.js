3d633097a0138e9b636b31727219a7d67315802d
Valtryek <amank225566@gmail.com>
2026-03-09 17:19:46 +0530

    sth-2
    Signed-off-by: Aman Ahmad <amank225566@gmail.com>

--- src/app.js
+++ src/app.js
@@ -1,11 +1,11 @@
 import express from "express";
 import cors from "cors";
-import testRoutes from "./routes/avmg.js";
+import avmgRoutes from "./routes/avmg.js";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use("/", testRoutes);
+app.use("/", avmgRoutes);
 
 export default app;
